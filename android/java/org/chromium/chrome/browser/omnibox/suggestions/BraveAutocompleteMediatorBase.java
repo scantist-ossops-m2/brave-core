e2e7e261f730ac93c1febff21702676a664b6d73
Artem Samoilenko <artem@brave.com>
2023-10-27 15:29:47 -0400

    [Android] New signature for AutocompleteMediator#loadUrlForOmniboxMatch
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/5799acb4fd93d6b94dc5834f1887af52503b49d2
    
    Remove unused `skipCheck` parameter from updateDestinationUrl call.
    
      🫧
    🫧🧽🧼🫧
        🫧
    
    Bug: 1490333

--- android/java/org/chromium/chrome/browser/omnibox/suggestions/BraveAutocompleteMediatorBase.java
+++ android/java/org/chromium/chrome/browser/omnibox/suggestions/BraveAutocompleteMediatorBase.java
@@ -16,17 +16,32 @@ import org.chromium.components.omnibox.OmniboxSuggestionType;
 import org.chromium.url.GURL;
 
 class BraveAutocompleteMediatorBase {
-    void loadUrlForOmniboxMatch(int matchIndex, @NonNull AutocompleteMatch suggestion,
-            @NonNull GURL url, long inputStart, boolean inVisibleSuggestionList,
+    void loadUrlForOmniboxMatch(
+            int matchIndex,
+            @NonNull AutocompleteMatch suggestion,
+            @NonNull GURL url,
+            long inputStart,
             boolean openInNewTab) {
-        BraveReflectionUtil.InvokeMethod(AutocompleteMediator.class, this, "loadUrlForOmniboxMatch",
-                int.class, matchIndex, AutocompleteMatch.class, suggestion, GURL.class, url,
-                long.class, inputStart, boolean.class, inVisibleSuggestionList, boolean.class,
+        BraveReflectionUtil.InvokeMethod(
+                AutocompleteMediator.class,
+                this,
+                "loadUrlForOmniboxMatch",
+                int.class,
+                matchIndex,
+                AutocompleteMatch.class,
+                suggestion,
+                GURL.class,
+                url,
+                long.class,
+                inputStart,
+                boolean.class,
                 openInNewTab);
         if (suggestion.getType() == OmniboxSuggestionType.SEARCH_WHAT_YOU_TYPED
                 || suggestion.getType() == OmniboxSuggestionType.SEARCH_SUGGEST) {
-            Context context = (Context) BraveReflectionUtil.getField(
-                    AutocompleteMediator.class, "mContext", this);
+            Context context =
+                    (Context)
+                            BraveReflectionUtil.getField(
+                                    AutocompleteMediator.class, "mContext", this);
             if (context != null && context instanceof BraveActivity) {
                 ((BraveActivity) context).getMiscAndroidMetrics().recordLocationBarQuery();
             }
