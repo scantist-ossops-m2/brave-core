030783a81461856a00c5ea5b027274ec802a895a
zenparsing <zenparsing@gmail.com>
2024-03-26 22:09:29 -0400

    Refactor parameters endpoint class

--- components/brave_rewards/core/endpoints/brave/get_parameters.cc
+++ components/brave_rewards/core/endpoints/brave/get_parameters.cc
@@ -5,163 +5,26 @@
 
 #include "brave/components/brave_rewards/core/endpoints/brave/get_parameters.h"
 
-#include <optional>
 #include <utility>
 #include <vector>
 
 #include "base/json/json_reader.h"
 #include "brave/components/brave_rewards/core/common/environment_config.h"
-#include "brave/components/brave_rewards/core/rewards_engine_impl.h"
+#include "brave/components/brave_rewards/core/common/url_loader.h"
 #include "net/http/http_status_code.h"
 
 namespace brave_rewards::internal::endpoints {
-using Error = GetParameters::Error;
-using Result = GetParameters::Result;
-
-namespace {
-
-Result ParseBody(RewardsEngineImpl& engine, const std::string& body) {
-  const auto value = base::JSONReader::Read(body);
-  if (!value || !value->is_dict()) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  const auto& dict = value->GetDict();
-
-  const auto rate = dict.FindDouble("batRate");
-  if (!rate) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-  auto parameters = mojom::RewardsParameters::New();
-  parameters->rate = *rate;
-
-  const auto auto_contribute_choice =
-      dict.FindDoubleByDottedPath("autocontribute.defaultChoice");
-  if (!auto_contribute_choice) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-  parameters->auto_contribute_choice = *auto_contribute_choice;
-
-  const auto* auto_contribute_choices =
-      dict.FindListByDottedPath("autocontribute.choices");
-  if (!auto_contribute_choices || auto_contribute_choices->empty()) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  for (const auto& choice : *auto_contribute_choices) {
-    if (choice.is_double() || choice.is_int()) {
-      parameters->auto_contribute_choices.push_back(choice.GetDouble());
-    }
-  }
-
-  const auto* tip_choices = dict.FindListByDottedPath("tips.defaultTipChoices");
-  if (!tip_choices || tip_choices->empty()) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  for (const auto& choice : *tip_choices) {
-    if (choice.is_double() || choice.is_int()) {
-      parameters->tip_choices.push_back(choice.GetDouble());
-    }
-  }
-
-  const auto* monthly_tip_choices =
-      dict.FindListByDottedPath("tips.defaultMonthlyChoices");
-  if (!monthly_tip_choices || monthly_tip_choices->empty()) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  for (const auto& choice : *monthly_tip_choices) {
-    if (choice.is_double() || choice.is_int()) {
-      parameters->monthly_tip_choices.push_back(choice.GetDouble());
-    }
-  }
-
-  const auto* payout_status = dict.FindDict("payoutStatus");
-  if (!payout_status) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  for (const auto [k, v] : *payout_status) {
-    if (v.is_string()) {
-      parameters->payout_status.emplace(k, v.GetString());
-    }
-  }
-
-  const auto* custodian_regions = dict.Find("custodianRegions");
-  if (!custodian_regions) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  auto wallet_provider_regions =
-      GetParameters::ValueToWalletProviderRegions(*custodian_regions);
-  if (!wallet_provider_regions) {
-    engine.LogError(FROM_HERE) << "Failed to parse body";
-    return base::unexpected(Error::kFailedToParseBody);
-  }
-
-  parameters->wallet_provider_regions = std::move(*wallet_provider_regions);
-
-  const auto* vbat_deadline = dict.FindString("vbatDeadline");
-  if (vbat_deadline) {
-    if (base::Time time;
-        base::Time::FromUTCString(vbat_deadline->c_str(), &time)) {
-      parameters->vbat_deadline = time;
-    }
-  }
-
-  const auto vbat_expired = dict.FindBool("vbatExpired");
-  if (vbat_expired) {
-    parameters->vbat_expired = *vbat_expired;
-  }
-
-  if (const auto tos_version = dict.FindInt("tosVersion")) {
-    parameters->tos_version = *tos_version;
-  }
-
-  return parameters;
-}
-
-}  // namespace
-
-// static
-Result GetParameters::ProcessResponse(RewardsEngineImpl& engine,
-                                      const mojom::UrlResponse& response) {
-  switch (response.status_code) {
-    case net::HTTP_OK:  // HTTP 200
-      return ParseBody(engine, response.body);
-    case net::HTTP_INTERNAL_SERVER_ERROR:  // HTTP 500
-      engine.LogError(FROM_HERE) << "Failed to get parameters";
-      return base::unexpected(Error::kFailedToGetParameters);
-    default:
-      engine.LogError(FROM_HERE)
-          << "Unexpected status code! (HTTP " << response.status_code << ')';
-      return base::unexpected(Error::kUnexpectedStatusCode);
-  }
-}
 
 GetParameters::GetParameters(RewardsEngineImpl& engine)
-    : RequestBuilder(engine) {}
+    : RewardsEngineHelper(engine) {}
 
 GetParameters::~GetParameters() = default;
 
-std::optional<std::string> GetParameters::Url() const {
-  return engine_->Get<EnvironmentConfig>()
-      .rewards_api_url()
-      .Resolve("/v1/parameters")
-      .spec();
-}
-
-mojom::UrlMethod GetParameters::Method() const {
-  return mojom::UrlMethod::GET;
+void GetParameters::Request(RequestCallback callback) {
+  Get<URLLoader>().Load(
+      CreateRequest(), URLLoader::LogLevel::kDetailed,
+      base::BindOnce(&GetParameters::OnResponse, weak_factory_.GetWeakPtr(),
+                     std::move(callback)));
 }
 
 std::optional<GetParameters::ProviderRegionsMap>
@@ -194,6 +57,137 @@ GetParameters::ValueToWalletProviderRegions(const base::Value& value) {
   }
 
   return regions_map;
+}
+
+mojom::UrlRequestPtr GetParameters::CreateRequest() {
+  auto request = mojom::UrlRequest::New();
+  request->method = mojom::UrlMethod::GET;
+  request->url = Get<EnvironmentConfig>()
+                     .rewards_api_url()
+                     .Resolve("/v1/parameters")
+                     .spec();
+  return request;
+}
+
+GetParameters::Result GetParameters::MapResponse(
+    const mojom::UrlResponse& response) {
+  if (!URLLoader::IsSuccessCode(response.status_code)) {
+    LogError(FROM_HERE) << "Unexpected status code: " << response.status_code;
+    return base::unexpected(Error::kUnexpectedStatusCode);
+  }
+
+  auto value = base::JSONReader::Read(response.body);
+  if (!value || !value->is_dict()) {
+    LogError(FROM_HERE) << "Invalid JSON";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  auto& dict = value->GetDict();
+
+  auto rate = dict.FindDouble("batRate");
+  if (!rate) {
+    LogError(FROM_HERE) << "Missing batRate";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  auto parameters = mojom::RewardsParameters::New();
+
+  parameters->rate = *rate;
+
+  auto auto_contribute_choice =
+      dict.FindDoubleByDottedPath("autocontribute.defaultChoice");
+  if (!auto_contribute_choice) {
+    LogError(FROM_HERE) << "Missing autocontribute.defaultChoice";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+  parameters->auto_contribute_choice = *auto_contribute_choice;
+
+  auto* auto_contribute_choices =
+      dict.FindListByDottedPath("autocontribute.choices");
+  if (!auto_contribute_choices || auto_contribute_choices->empty()) {
+    LogError(FROM_HERE) << "Missing autocontribute.choices";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  for (auto& choice : *auto_contribute_choices) {
+    if (choice.is_double() || choice.is_int()) {
+      parameters->auto_contribute_choices.push_back(choice.GetDouble());
+    }
+  }
+
+  auto* tip_choices = dict.FindListByDottedPath("tips.defaultTipChoices");
+  if (!tip_choices || tip_choices->empty()) {
+    LogError(FROM_HERE) << "Missing tips.defaultTipChoices";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  for (auto& choice : *tip_choices) {
+    if (choice.is_double() || choice.is_int()) {
+      parameters->tip_choices.push_back(choice.GetDouble());
+    }
+  }
+
+  auto* monthly_tip_choices =
+      dict.FindListByDottedPath("tips.defaultMonthlyChoices");
+  if (!monthly_tip_choices || monthly_tip_choices->empty()) {
+    LogError(FROM_HERE) << "Missing tips.defaultMonthlyChoices";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  for (auto& choice : *monthly_tip_choices) {
+    if (choice.is_double() || choice.is_int()) {
+      parameters->monthly_tip_choices.push_back(choice.GetDouble());
+    }
+  }
+
+  auto* payout_status = dict.FindDict("payoutStatus");
+  if (!payout_status) {
+    LogError(FROM_HERE) << "Missing payoutStatus";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  for (auto [k, v] : *payout_status) {
+    if (v.is_string()) {
+      parameters->payout_status.emplace(k, v.GetString());
+    }
+  }
+
+  auto* custodian_regions = dict.Find("custodianRegions");
+  if (!custodian_regions) {
+    LogError(FROM_HERE) << "Missing custodianRegions";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  auto wallet_provider_regions =
+      ValueToWalletProviderRegions(*custodian_regions);
+  if (!wallet_provider_regions) {
+    LogError(FROM_HERE) << "Invalid custodianRegions";
+    return base::unexpected(Error::kFailedToParseBody);
+  }
+
+  parameters->wallet_provider_regions = std::move(*wallet_provider_regions);
+
+  if (auto* vbat_deadline = dict.FindString("vbatDeadline")) {
+    if (base::Time time;
+        base::Time::FromUTCString(vbat_deadline->c_str(), &time)) {
+      parameters->vbat_deadline = time;
+    }
+  }
+
+  if (auto vbat_expired = dict.FindBool("vbatExpired")) {
+    parameters->vbat_expired = *vbat_expired;
+  }
+
+  if (auto tos_version = dict.FindInt("tosVersion")) {
+    parameters->tos_version = *tos_version;
+  }
+
+  return parameters;
+}
+
+void GetParameters::OnResponse(RequestCallback callback,
+                               mojom::UrlResponsePtr response) {
+  std::move(callback).Run(MapResponse(*response));
 }
 
 }  // namespace brave_rewards::internal::endpoints
