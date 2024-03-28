a7e6f1e15654579585f6424d45dde36f8c81d11d
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2024-03-28 13:12:16 +0000

    perf(wallet): do not wait for sign message reqs when initing panel

--- components/brave_wallet_ui/panel/actions/wallet_panel_actions.ts
+++ components/brave_wallet_ui/panel/actions/wallet_panel_actions.ts
@@ -4,11 +4,7 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import { createAction } from '@reduxjs/toolkit'
-import {
-  ShowConnectToSitePayload,
-  SignMessageProcessedPayload,
-  SignMessageHardwarePayload
-} from '../constants/action_types'
+import { ShowConnectToSitePayload } from '../constants/action_types'
 import { BraveWallet, PanelTypes } from '../../constants/types'
 import { HardwareWalletResponseCodeType } from '../../common/hardware/types'
 
@@ -19,16 +15,6 @@ export const setupWallet = createAction('setupWallet')
 export const openWalletSettings = createAction('openWalletSettings')
 export const navigateTo = createAction<PanelTypes>('navigateTo')
 export const navigateToMain = createAction('navigateToMain')
-export const signMessage =
-  createAction<BraveWallet.SignMessageRequest[]>('signMessage')
-export const signMessageProcessed = createAction<SignMessageProcessedPayload>(
-  'signMessageProcessed'
-)
-export const signMessageHardware = createAction<SignMessageHardwarePayload>(
-  'signMessageHardware'
-)
-export const signMessageHardwareProcessed =
-  createAction<SignMessageProcessedPayload>('signMessageHardwareProcessed')
 export const setHardwareWalletInteractionError = createAction<
   HardwareWalletResponseCodeType | undefined
 >('setHardwareWalletInteractionError')
@@ -39,9 +25,4 @@ export const setSelectedTransactionId = createAction<string | undefined>(
 )
 export const setCloseOnDeactivate = createAction<boolean>(
   'setCloseOnDeactivate'
-)
-export const signMessageError =
-  createAction<BraveWallet.SignMessageError[]>('signMessageError')
-export const signMessageErrorProcessed = createAction<string>(
-  'signMessageErrorProcessed'
 )
