debdecb10fc1c17d42e9cbca6efd4d4ff3bea2ed
Stephen Heaps <steaps.4@gmail.com>
2024-04-04 15:18:47 -0400

    Fix colour schemes, white flash when launching in dark mode

--- ios/brave-ios/Sources/BraveWallet/Crypto/CryptoView.swift
+++ ios/brave-ios/Sources/BraveWallet/Crypto/CryptoView.swift
@@ -396,6 +396,11 @@ struct ChromeWebView: UIViewControllerRepresentable {
     return ChromeWebViewController(privateBrowsing: false).then {
       $0.title = title
       $0.loadURL(urlString)
+      $0.view.backgroundColor = UIColor(braveSystemName: .containerBackground)
+      $0.webView.backgroundColor = UIColor(braveSystemName: .containerBackground)
+      $0.webView.scrollView.backgroundColor = UIColor(braveSystemName: .containerBackground)
+      // WKWebView flashes white screen on load regardless of background colour assignments
+      $0.webView.isOpaque = false
       if #available(iOS 16.4, *) {
         $0.webView.isInspectable = true
       }
@@ -416,6 +421,7 @@ struct WebUISendView: View {
     ChromeWebView(title: Strings.Wallet.send, urlString: "brave://wallet/send")
       .navigationTitle(Strings.Wallet.send)
       .navigationBarTitleDisplayMode(.inline)
+      .background(Color(braveSystemName: .containerBackground))
   }
 }
 
@@ -425,5 +431,6 @@ struct WebUISwapView: View {
     ChromeWebView(title: Strings.Wallet.swap, urlString: "brave://wallet/swap")
       .navigationTitle(Strings.Wallet.swap)
       .navigationBarTitleDisplayMode(.inline)
+      .background(Color(braveSystemName: .containerBackground))
   }
 }
