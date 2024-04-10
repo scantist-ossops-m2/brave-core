8584d97057d6a911f5469ff7f91cad3b5f86b425
bridiver <34129+bridiver@users.noreply.github.com>
2024-04-10 09:13:39 -0700

    remove unused header

--- browser/brave_prefs_browsertest.cc
+++ browser/brave_prefs_browsertest.cc
@@ -5,7 +5,6 @@
 
 #include "base/feature_list.h"
 #include "brave/browser/ethereum_remote_client/buildflags/buildflags.h"
-#include "brave/browser/metrics/buildflags/buildflags.h"
 #include "brave/browser/metrics/pref_names.h"
 #include "brave/components/brave_rewards/common/pref_names.h"
 #include "brave/components/brave_shields/core/common/features.h"
