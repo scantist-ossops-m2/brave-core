5c5bc31c69de6fbbf90ea0c5c13727cdada9f9e2
bridiver <34129+bridiver@users.noreply.github.com>
2024-04-10 17:28:35 -0700

    fix gn check

--- browser/first_run/first_run.cc
+++ browser/first_run/first_run.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/first_run/first_run.h"
+#include "brave/browser/first_run/first_run.h"
 
 #include "base/command_line.h"
 #include "base/notreached.h"
